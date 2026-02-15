9a5e92f934f34cb61ca75d58568435796fb9cc85
rennangimenez <rennan.colozzo18@hotmail.com>
2026-02-15 02:38:08 -0300

    change test title

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -2,5 +2,6 @@ import { test, expect } from '@playwright/test'
 
 test('the webapp should be online', async ({ page }) => {
   await page.goto('http://localhost:5173')
+  
   await expect(page).toHaveTitle(/Velô by Papito/)
 })
