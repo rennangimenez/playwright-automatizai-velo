cf3e9fa84b54cd1d87edd310ddb3c4e0d11358ef
rennangimenez <rennan.colozzo18@hotmail.com>
2026-03-02 22:46:53 -0300

    chore: AAA - Arrange, Act, Assert

--- playwright/e2e/orders.spec.ts
+++ playwright/e2e/orders.spec.ts
@@ -1,22 +1,23 @@
 import { test, expect } from '@playwright/test'
 
+// AAA - Arrange, Act, Assert
+
 test('should consult one approved order', async ({ page }) => {
+  // Arrange
   await page.goto('http://localhost:5173/')
-  
-  // Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
 
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-  // Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
-  await page.getByTestId('search-order-id').fill('VLO-0PDM07')
 
+  // Act
+  await page.getByTestId('search-order-id').fill('VLO-0PDM07')
   await page.getByTestId('search-order-button').click()
 
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-0PDM07')
-
-  await expect(page.getByTestId('order-result-status')).toBeVisible();
+  
+  await expect(page.getByTestId('order-result-status')).toBeVisible()
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
 })
